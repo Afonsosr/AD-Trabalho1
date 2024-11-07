52766a73397acc818501f4d7225f5aa749f789f8
Afonsosr <113983116+Afonsosr@users.noreply.github.com>
2024-11-07 12:50:00 +0000

    Add files via upload

---
+++ Farmaceutico.java
@@ -0,0 +1,51 @@
+import java.io.Serializable;
+import java.util.UUID;
+
+public class Farmaceutico implements Serializable {
+    private String id;
+    private String nome;
+    private String telefone;
+
+
+    public Farmaceutico(String id, String nome, String telefone) {
+        this.id = UUID.randomUUID().toString();
+        this.nome = nome;
+        this.telefone = telefone;
+
+    }
+
+    public Farmaceutico() {
+        this.id = UUID.randomUUID().toString();
+    }
+
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getTelefone() {
+        return telefone;
+    }
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    @Override
+    public String toString() {
+        return "Contacto{" +
+                "id='" + id + '\'' +
+                "nome='" + nome + '\'' +
+                ", telefone='" + telefone + '\'' +
+                '}';
+    }
+
+}
+
+
---
+++ Medicamento.java
@@ -0,0 +1,59 @@
+import java.io.Serializable;
+import java.util.UUID;
+
+public class Medicamento implements Serializable {
+    private String id;
+    private String nome;
+    private String fornecedor;
+    private Integer stock;
+
+    public Medicamento(String id, String nome, String fornecedor, Integer stock) {
+        this.id = UUID.randomUUID().toString();
+        this.nome = nome;
+        this.fornecedor = fornecedor;
+        this.stock = stock;
+    }
+
+    public Medicamento() {
+        this.id = UUID.randomUUID().toString();
+    }
+
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public Integer getStock() {
+        return stock;
+    }
+    public void setStock(Integer stock) {
+        this.stock = stock;
+    }
+    public String getFornecedor() {
+        return fornecedor;
+    }
+    public void setFornecedor(String fornecedor) {
+        this.fornecedor = fornecedor;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Contacto{" +
+                "id='" + id + '\'' +
+                "nome='" + nome + '\'' +
+                ", fornecedor='" + fornecedor + '\'' +
+                ", stock=" + stock +
+                '}';
+    }
+
+}
+
+
