3786cfef477e3bec38a978ace144a55573622fc8
a95074 <92465292+a95074@users.noreply.github.com>
2024-11-11 16:47:54 +0000

    Add files via upload

---
+++ Codigo-sem-erros/APIGestorUtentes.java
@@ -0,0 +1,18 @@
+package Trabalho;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public interface APIGestorUtentes extends Remote {
+    public Utente createUtente(String nome, String genero, LocalDate dataNascimento, ArrayList<String> medicacao) throws RemoteException;
+
+    public Utente getUtente(String id) throws RemoteException;
+
+    public List<String> procuraUtente(String nome) throws RemoteException;
+
+    public void alteraMedicacao(String id, ArrayList<String> medicacao) throws RemoteException;
+
+}
