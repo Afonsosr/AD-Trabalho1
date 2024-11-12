66aabcc37f5d0c6dd13d52b12cd063f6ee7a3581
a95074 <92465292+a95074@users.noreply.github.com>
2024-11-12 12:41:06 +0000

    Update GestorMedicamentos.java

--- Codigo-sem-erros/GestorMedicamentos.java
+++ Codigo-sem-erros/GestorMedicamentos.java
@@ -1,6 +1,5 @@
 package Trabalho;
 
-import javax.print.attribute.standard.Media;
 import java.io.Serializable;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
@@ -9,7 +8,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class GestorMedicamentos extends UnicastRemoteObject implements Serializable{
+public class GestorMedicamentos extends UnicastRemoteObject implements Serializable {
 
     private Map<String, Medicamento> medicamentos;
 
@@ -18,17 +17,20 @@ public class GestorMedicamentos extends UnicastRemoteObject implements Serializa
         medicamentos = new HashMap<>();
     }
 
-    public Medicamento createMedicamento(String id, String nome, String fornecedor) {
-        Medicamento nm = new Medicamento(nome, fornecedor);
+    // Criação de medicamento com ID gerado automaticamente
+    public Medicamento createMedicamento(String nome, String fornecedor, Integer stock) {
+        Medicamento nm = new Medicamento(nome, fornecedor, stock);
         medicamentos.put(nm.getId(), nm);
         return nm;
     }
 
-    public Medicamento getMedicamento(String id) {return medicamentos.get(id);}
+    public Medicamento getMedicamento(String id) {
+        return medicamentos.get(id);
+    }
 
     public List<String> procuraMedicamento(String nome) {
         List<String> res = new ArrayList<>();
-        for(Medicamento m : this.medicamentos.values()) {
+        for (Medicamento m : this.medicamentos.values()) {
             if (m.getNome().contains(nome)) {
                 res.add(m.getId());
             }
@@ -37,16 +39,33 @@ public class GestorMedicamentos extends UnicastRemoteObject implements Serializa
     }
 
     public void alteraFornecedor(String id, String fornecedor) {
-        if(medicamentos.containsKey(id)) {
+        if (medicamentos.containsKey(id)) {
             medicamentos.get(id).setFornecedor(fornecedor);
         }
     }
 
-    public void alteraStock(String id, Integer stock) {
-        if(medicamentos.containsKey(id)) {                         // aqui usamos tanto quando há dispensa/uso como quando ha aquisicao
-            medicamentos.get(id).setStock(stock);
+    // Método para aumentar o stock de um medicamento
+    public void aumentaStock(String id, Integer quantidade) {
+        if (medicamentos.containsKey(id)) {
+            Medicamento med = medicamentos.get(id);
+            med.setStock(med.getStock() + quantidade);
         }
     }
+
+    // Método para reduzir o stock de um medicamento (para consumo/uso)
+    public void reduzStock(String id, Integer quantidade) {
+        if (medicamentos.containsKey(id)) {
+            Medicamento med = medicamentos.get(id);
+            int novoStock = med.getStock() - quantidade;
+            if (novoStock >= 0) {
+                med.setStock(novoStock);
+            } else {
+                System.out.println("Erro: Quantidade insuficiente em estoque.");
+            }
+        }
+    }
+}
+
 
 
 
